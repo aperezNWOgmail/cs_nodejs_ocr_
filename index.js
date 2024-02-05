3cab34297596b5b42e099a5f8ef59f9eef4c2852
alejandro.perez.acosta@hotmail.com <aperezNWO>
2024-02-05 01:31:22 -0500

    upload image from base64 stream

--- index.js
+++ index.js
@@ -3,32 +3,72 @@
 //
 const _express = require('express');
 const app      = _express();
+const cors     = require('cors');
 const port     = 3000;
 const fs       = require("fs");
+const path     = require('path');
 const _multer  = require('multer');
 const _upload  = _multer({ dest: 'img/signatures/dest/' }); // Store uploaded images in 'uploads' directory
+
+app.use(_express.json());
+
+app.use(cors());
 //----------------------------------
 //----------------------------------
 //
-function doOcr(p_imagePath) { 
+function doOcr(p_imagePath, res) { 
     //
     const Tesseract = require('tesseract.js');
     //
-    async function recognizeText(imagePath) {
+    async function recognizeText(imagePath, res) {
       const { data: { text } } = await Tesseract.recognize(imagePath, 'eng');
       const msg = "texto en imagen :" + text;
       console.debug(msg);
-      return msg;
+      res.status(200).json({ message: msg });
     }
     //
-    return recognizeText(p_imagePath);
+    return recognizeText(p_imagePath, res);
 }
 //
-app.post('/upload', _upload.single('image'), (req, res) => {
+app.post('/uploadFromUIX', _upload.single('image'), (req, res) => {
   const imagePath = req.file.path; // Access path of uploaded image
   console.debug('Image uploaded successfully:', imagePath);
   res.send('Image uploaded successfully');
   const msg       = doOcr(imagePath);
+});
+//
+app.post('/upload', (req, res) => {
+
+  // Handle the base64 image data as needed (e.g., save to disk, process, etc.)
+  // ...
+  // Base64-encoded image data
+  const base64Image = req.body.base64Image;
+
+  // Extract the file extension and data from the base64 string
+  const matches       = base64Image.match(/^data:image\/([A-Za-z-+/]+);base64,(.+)$/);
+  const fileExtension = matches[1];
+  const base64Data    = matches[2];
+
+  // Create a buffer from the base64 data
+  const imageBuffer   = Buffer.from(base64Data, 'base64');
+
+  // Create a unique filename based on timestamp
+  const filename      = `image_${Date.now()}.${fileExtension}`;
+
+  // Specify the file path where the image will be saved
+  //const filePath      = path.join('img/signatures/dest/', 'images', filename); // Adjust the directory as needed
+  const filePath      = path.join('img/signatures/', 'dest', filename); // Adjust the directory as needed
+
+  // Write the buffer to a file
+  fs.writeFile(filePath, imageBuffer, (err) => {
+    if (err) {
+      console.error('Error saving image:', err);
+    } else {
+      console.log('Image saved successfully:', filePath);
+    }
+  });
+  //
+  doOcr(filePath, res);
 });
 // index
 async function GetIndex() {
